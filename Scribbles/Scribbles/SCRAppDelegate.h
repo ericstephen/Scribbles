6df55b250bab82d770a0e65d15dc408a3d45f232
ericstephen <EricWilliams@Erics-Macbook-Pro.local>
2014-08-04 09:17:44 -0400

    create app

---
+++ Scribbles/Scribbles/SCRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCRAppDelegate.h
+//  Scribbles
+//
+//  Created by Eric Williams on 8/4/14.
+//  Copyright (c) 2014 Eric Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
